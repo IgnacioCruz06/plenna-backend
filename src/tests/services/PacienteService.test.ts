f9e6607a03374495b576785091a3725df1d5ed05
José Ignacio Cruz Reyes <117957482+IgnacioCruz06@users.noreply.github.com>
2025-04-06 23:08:49 -0600

    chore: finish test cases for Paciente CRUD

--- src/tests/services/PacienteService.test.ts
+++ src/tests/services/PacienteService.test.ts
@@ -1,7 +1,6 @@
 import request from "supertest";
 import app from "../../server";
-import { setupDB } from "../../db";
-import mongoose from "mongoose";
+import { connectDBTest, disconnectDBTest } from "../setupDbTest";
 
 const mockPaciente = {
   firstName: "Test",
@@ -17,14 +16,17 @@ const mockPaciente = {
 };
 
 describe("Testing functions for CRUD operations in PacienteService.ts file", () => {
-  beforeEach(async () => {
-    setupDB();
+  beforeAll(async () => {
+    await connectDBTest();
   });
 
   afterAll(async () => {
-    await mongoose.disconnect();
+    await disconnectDBTest();
   });
 
+  let testId;
+
+  /* --- Start GET all test cases --- */
   it("GET /api/v1/paciente works", async () => {
     const response = await request(app).get("/api/v1/paciente").send();
 
@@ -34,6 +36,7 @@ describe("Testing functions for CRUD operations in PacienteService.ts file", () 
     expect(response.body).toHaveProperty("data");
   });
 
+  /* --- Start POST test cases --- */
   it("POST /api/v1/paciente works", async () => {
     const response = await request(app)
       .post("/api/v1/paciente")
@@ -45,12 +48,111 @@ describe("Testing functions for CRUD operations in PacienteService.ts file", () 
     expect(response.body).toHaveProperty("data");
   });
 
-  it("POST /api/v1/paciente create a new patiente correctly", async () => {
+  it("POST /api/v1/paciente create a new patient correctly", async () => {
     const response = await request(app)
       .post("/api/v1/paciente")
       .send(mockPaciente);
 
-    expect(response.body._id).toBeDefined();
-    expect(response.body.email).toBe("test.jest@example.com");
+    expect(response.body.data._id).toBeDefined();
+    expect(response.body.data.email).toBe("test.jest@example.com");
+    testId = response.body.data._id;
+  });
+
+  it("POST /api/v1/paciente bad request", async () => {
+    const response = await request(app)
+      .post("/api/v1/paciente")
+      .send({
+        firstName: "Test",
+        lastName: "Jest",
+        birthDate: new Date("1989-08-04"),
+        gender: "k",
+        email: "test.jest@example.com",
+        phone: "5551234345",
+        address: "",
+        bloodType: "O+",
+        allergies: [],
+        chronicDiseases: ["Hipertensión"],
+      });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("message", "Bad Request");
+    expect(response.body).toHaveProperty("data");
+    expect(response.body.data[0].message).toBe(
+      '"gender" must be one of [F, M, Other]'
+    );
+  });
+
+  /* --- Start GET by id test cases --- */
+  it("GET /api/v1/paciente/:id works", async () => {
+    const response = await request(app)
+      .get(`/api/v1/paciente/${testId}`)
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.headers["content-type"]).toContain("json");
+    expect(response.body).toHaveProperty("message", "Ok");
+    expect(response.body).toHaveProperty("data");
+    expect(response.body.data._id).toBe(testId);
+  });
+
+  it("GET /api/v1/paciente/:id not found", async () => {
+    const response = await request(app)
+      .get(`/api/v1/paciente/777252B2ec5Ee6a22a8eDc34`)
+      .send();
+
+    expect(response.status).toBe(404);
+    expect(response.headers["content-type"]).toContain("json");
+    expect(response.body).toHaveProperty("message", "Not Found");
+  });
+
+  it("GET /api/v1/paciente/:id Invalid ID format", async () => {
+    const response = await request(app).get(`/api/v1/paciente/77`).send();
+
+    expect(response.status).toBe(400);
+    expect(response.headers["content-type"]).toContain("json");
+    expect(response.body).toHaveProperty("message", "Bad Request");
+    expect(response.body).toHaveProperty("data", "Invalid ID format.");
+  });
+
+  /* --- Start PATCH test cases --- */
+  it("PATCH /api/v1/paciente/:id works", async () => {
+    const response = await request(app)
+      .patch(`/api/v1/paciente/${testId}`)
+      .send({ email: "test.jest_updated@email.com" });
+
+    expect(response.status).toBe(200);
+    expect(response.headers["content-type"]).toContain("json");
+    expect(response.body).toHaveProperty("message", "Ok");
+    expect(response.body).toHaveProperty("data");
+    expect(response.body.data.email).toBe("test.jest_updated@email.com");
+  });
+
+  it("PATCH /api/v1/paciente/:id not found", async () => {
+    const response = await request(app)
+      .patch(`/api/v1/paciente/777252B2ec5Ee6a22a8eDc34`)
+      .send();
+
+    expect(response.status).toBe(404);
+    expect(response.headers["content-type"]).toContain("json");
+    expect(response.body).toHaveProperty("message", "Not Found");
+  });
+
+  /* --- Start DELETE test cases --- */
+  it("DELETE /api/v1/paciente/:id works", async () => {
+    const response = await request(app)
+      .delete(`/api/v1/paciente/${testId}`)
+      .send();
+
+    expect(response.status).toBe(204);
+  });
+
+  it("DELETE /api/v1/paciente/:id not found", async () => {
+    const response = await request(app)
+      .delete(`/api/v1/paciente/${testId}`)
+      .send();
+
+    expect(response.status).toBe(404);
+    expect(response.headers["content-type"]).toContain("json");
+    expect(response.body).toHaveProperty("message", "Not Found");
   });
 });
